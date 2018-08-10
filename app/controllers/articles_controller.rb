f133692f4537d6410962729df3d960a2248167e0
Errol <erroli8a8@gmail.com>
2018-08-10 07:52:37 +0000

    Install boostrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -44,8 +44,7 @@ class ArticlesController < ApplicationController
   def destroy
     
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
-    redirect_to articles_path
+    redirect_to articles_path, flash: { notice: "Article was successfully deleted." }
   end
   
   private
